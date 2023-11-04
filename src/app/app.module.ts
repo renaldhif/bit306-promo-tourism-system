bfa034f469b45f008cbd9907450ba5569a818990
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-11-04 15:53:14 +0800

    new route, add login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/landing-page/header/header.component';
+import { FooterComponent } from './components/landing-page/footer/footer.component';
+import { MainComponent } from './components/landing-page/main/main.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    LandingPageComponent,
+    MainComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
   ],
   imports: [
     BrowserModule,
