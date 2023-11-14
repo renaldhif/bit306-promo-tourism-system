fcd3be7cc49e7219fc056704dc269c12beed5ff0
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-11-15 00:16:00 +0800

    update color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {
-      screens:{
-        xs: '320px',
-      },
-      colors:{
-        // base colors
-        primary: '#007DFC',
-        darkPrimary: '#0069D9',
-        secondary: '#DEE8FF',
+export const content = [
+  "./src/**/*.{html,ts}",
+]
+export const theme = {
+  extend: {
+    screens: {
+      xs: '320px',
+    },
+    colors: {
+      // base colors
+      primary: '#007DFC',
+      darkPrimary: '#0069D9',
+      secondary: '#DEE8FF',
 
-        // additional colors
-        darkGrey: '#6b7280',
-        whiteBackground: '#ffffff',
-        backgroundHover: '#f3f4f6',
-        border: '#DDE2E4',
+      // additional colors
+      lightPrimaryBackground: '#3097FF',
+      darkGrey: '#6b7280',
+      whiteBackground: '#ffffff',
+      backgroundHover: '#f3f4f6',
+      border: '#DDE2E4',
 
         // text colors
         textPrimary: '#374151',
-      },
     },
   },
   plugins: [],
