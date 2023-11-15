0c92501c8efc0b3dc84416db5656929d0d83c0bf
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-11-15 14:37:40 +0800

    fix login and add swal show validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,7 @@ export class LoginComponent {
   loginForm: FormGroup;
   error: string = '';
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
@@ -20,6 +22,9 @@ export class LoginComponent {
   buttonClicked = false;
 
   onSubmit() {
+    console.log('Login button pressed');
+    console.log('apakah valid?' + this.loginForm.valid);
+
     this.buttonClicked = true;
     if (this.loginForm.valid) {
       const email = this.loginForm.get('email')?.value;
@@ -28,15 +33,54 @@ export class LoginComponent {
       // Replace this with your actual form submission logic
       if (email === 'chandra@gmail.com' && password === 'password') {
         this.error = ''; // Clear any previous error message
-        console.log('Login successful!'); // You can redirect to another page or perform other actions here
+        Swal.fire({
+          title: 'Login Success',
+          text: 'Welcome to the dashboard',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then(() => {
+          // Navigate to the merchant dashboard [TEMPORARY!]
+          this.router.navigate(['/merchant/dashboard'], { replaceUrl: true });
+        });
       } else {
         this.error = 'Login failed. Invalid email or password.';
-        console.log(this.error); // Log the error message
+        console.log(this.error);
+        Swal.fire({
+          title: 'Login Failed',
+          text: 'Invalid email or password',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
       }
     } else {
-      this.error = 'Login failed. Invalid email or password.';
+      if (this.loginForm.get('email')?.hasError('required') || this.loginForm.get('password')?.hasError('required')) {
+        this.error = 'Login failed. Please fill in all the required fields.';
+        Swal.fire({
+          title: 'Empty Fields',
+          text: 'Please fill in all the required fields',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      } else if (this.loginForm.get('email')?.hasError('email')) {
+        this.error = 'Login failed. Invalid email format.';
+        Swal.fire({
+          title: 'Invalid Email',
+          text: 'Please enter a valid email address',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      } else {
+        // this.error = 'Login failed. Invalid email or password.';
+        Swal.fire({
+          title: 'Login Failed',
+          text: 'Invalid email or password',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
     }
   }
+
 
 
 
