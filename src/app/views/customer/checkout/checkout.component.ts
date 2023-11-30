e83abc3ca1d2b26637f76b57366af6d73ce15450
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-11-30 21:50:22 +0800

    add checkout

---
+++ src/app/views/customer/checkout/checkout.component.ts
@@ -0,0 +1,148 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/service/product-service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent {
+
+  checkoutForm: FormGroup;
+  orderItem: any;
+  productId: number | null;
+  quantity:number = 1;
+
+  ngOnInit(): void {
+    if (this.productId !== null) {
+      this.orderItem = this.productService.getUserOrderProduct(this.productId);
+    }
+  }
+
+  constructor(private router:Router, private formBuilder: FormBuilder, private route: ActivatedRoute, private productService: ProductService) {
+    this.checkoutForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      paymentMethod: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      notes: ['', []],
+    });
+    const idParam = this.route.snapshot.paramMap.get('id');
+    this.productId = idParam ? parseInt(idParam, 10) : null;
+
+    // might not appropriate to use query parameters here to pass the quantity
+    // later we will use a service or state management to store the quantity
+    this.route.queryParams.subscribe((params) => {
+      this.quantity = +params['quantity'];
+    });
+
+    // intialize quantity to 1 if no query parameter is passed
+    if (this.route.snapshot.queryParamMap.get('quantity') == null) {
+      this.quantity = 1;
+    }
+  }
+
+  convertToDisplayText(key: any): string {
+    return key.replace(/(\d)/g, ' $1');
+  }
+
+  onBack() {
+    // go back to previous page
+    Swal.fire({
+      title: 'Warning',
+      text: 'Are you sure you want to go back?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No'
+    }).then
+    (result => {
+      if (result.isConfirmed) {
+        window.history.back();
+      }
+    })
+  }
+
+  onSubmit() {
+    if (this.checkoutForm.valid) {
+      // Handle form submission and payment method selection
+      const formData = this.checkoutForm.value;
+      Swal.fire({
+        title: 'Success',
+        text: 'Your order has been placed with data: ' + JSON.stringify(formData, null,2 ),
+        icon: 'success',
+        confirmButtonText: 'OK'
+      }).then
+      (result => {
+        if (result.isConfirmed && formData.paymentMethod == 'paypal') {
+          Swal.fire({
+            title: 'Opening paypal payment page...',
+            text: 'Navigating to paypal payment page...',
+            icon: 'info',
+            confirmButtonText: 'OK'
+          }).then
+          (result => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: 'Success',
+                text: 'This condition is after payment has been made \n Booking success!!!',
+                icon: 'success',
+                confirmButtonText: 'OK'
+              }).then
+              (result => {
+                if (result.isConfirmed) {
+                  this.router.navigate(['/customer/payment-history']);
+                }
+              })
+            }
+          })
+        }
+        else if (result.isConfirmed && formData.paymentMethod == 'creditcard') {
+          Swal.fire({
+            title: 'Opening credit card payment page...',
+            text: 'Navigating to credit card payment page...',
+            icon: 'info',
+            confirmButtonText: 'OK'
+          }).then
+          (result => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: 'Success',
+                text: 'This condition is after payment has been made \n Booking success!!!',
+                icon: 'success',
+                confirmButtonText: 'OK'
+              }).then
+              (result => {
+                if (result.isConfirmed) {
+                  this.router.navigate(['/customer/payment-history']);
+                }
+              })
+            }
+          })
+        }
+        else{
+          Swal.fire({
+            title: 'Error',
+            text: 'Please select payment method',
+            icon: 'error',
+            confirmButtonText: 'OK'
+          });
+        }
+      })
+    }
+    else{
+      Swal.fire({
+        title: 'Error',
+        text: 'Please fill in all the required fields',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      });
+    }
+  }
+
+}
