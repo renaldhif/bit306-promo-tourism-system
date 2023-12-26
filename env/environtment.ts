5efe8cef5a74004406e62e2f3d3b86946fedad87
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-12-27 03:08:07 +0800

    paypal window works on fe

--- env/environtment.ts
+++ env/environtment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://localhost:3000',
+  paypalClientId: 'AePxdocBthN9qugsuUjsH_hKLFDicWJDDwx3kBsxxDblG-_zqocRFQc7QBqThQ_KJn9UKz7U8zY2Za5d',
+  FIXER_IO_CURRENCY_API_KEY: 'cc7a29719b3885421f4a7164aa2fdda0',
 };
--- src/app/views/product/product.component.ts
+++ src/app/views/product/product.component.ts
@@ -1,74 +1,3 @@
-// import { Component } from '@angular/core';
-// import { ActivatedRoute } from '@angular/router';
-// import { ProductService } from 'src/app/service/product-service';
-// import { Router } from '@angular/router';
-// import Swal from 'sweetalert2';
-// @Component({
-//   selector: 'app-product',
-//   templateUrl: './product.component.html',
-//   styleUrls: ['./product.component.css']
-// })
-// export class ProductComponent {
-
-//   productId: number | null;
-//   product: any;
-//   quantity: number = 1;
-//   pricePerItem: number = 0;
-//   destinationsKeys: string[] = [];
-//   reviews: any[] = [];
-//   includes: string[] = [];
-
-//   constructor(private route: ActivatedRoute, private router:Router, private productService: ProductService) {
-//     const idParam = this.route.snapshot.paramMap.get('id');
-//     this.productId = idParam ? parseInt(idParam, 10) : null;
-//   }
-
-//   ngOnInit(): void {
-//     if (this.productId !== null) {
-//       this.product = this.productService.getProductById(this.productId);
-//       this.pricePerItem = this.product.price;
-//       this.destinationsKeys = Object.keys(this.product.destinations);
-//       this.reviews = Object.values(this.product.reviews);
-//       this.includes = Object.values(this.product.includes);
-//     }
-//   }
-
-//   // Calculate the total price based on quantity and price per item
-//   get total(): number {
-//     return this.quantity * this.pricePerItem;
-//   }
-
-//   // Increase the quantity
-//   incrementQuantity() {
-//     // limit to 99
-//     if (this.quantity < 99) {
-//       this.quantity++;
-//     }
-//   }
-
-//   // Decrease the quantity, but ensure it doesn't go below 1
-//   decrementQuantity() {
-//     if (this.quantity > 1) {
-//       this.quantity--;
-//     }
-//   }
-
-//   checkoutNow(productID: number) {
-//     // might not appropriate to use query parameters here to pass the quantity
-//     // later we will use a service or state management to store the quantity
-//     const queryParams = {
-//       quantity: this.quantity,
-//     };
-
-//     // Navigate to /customer/checkout/{productID} with query parameters
-//     this.router.navigate(['/customer/checkout', productID], {
-//       queryParams,
-//     });
-//   }
-
-// }
-
-
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/service/product-service';
@@ -107,33 +36,6 @@ export class ProductComponent {
     console.log('this product id' + this.productId);
 
   }
-
-  // ngOnInit(): void {
-  //   if (this.productId !== null) {
-  //     console.log("jalan di if tidak null")
-  //     this.productService.getProductById(this.productId).subscribe(
-  //       (product) => {
-  //         console.log(product);
-  //         this.product = product;
-  //         this.pricePerItem = product.price;
-  //         this.destinationsKeys = Object.keys(this.product.destinations);
-  //         this.reviews = Object.values(this.product.reviews);
-  //         this.includes = Object.values(this.product.includes);
-  //       },
-  //       (error) => {
-  //         console.error('Error fetching product details:', error);
-  //         // Handle the error, e.g., show an error message to the user
-  //       }
-  //     );
-  //   }
-  //   else {
-  //     Swal.fire({
-  //       icon: 'error',
-  //       title: 'Oops...',
-  //       text: 'Something went wrong!',
-  //     });
-  //   }
-  // }
 
   ngOnInit(): void {
     if (this.productId !== null) {
@@ -216,7 +118,6 @@ export class ProductComponent {
     // Navigate to /customer/checkout/{productID} without using query parameters
     this.router.navigate(['/customer/checkout', productID]);
   }
-
 }
 
 
