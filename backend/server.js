4831cd9f7ea17d778128a8674fcae04491d74c5e
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-12-28 03:31:21 +0800

    handle block cors if needed

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,39 @@ const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Middlewares
+// const corsOptions = {
+//   origin: 'http://localhost:4200/',
+//   optionsSuccessStatus: 200
+// };
+
+// app.use(cors(corsOptions));
+
 app.use(cors());
 app.use(bodyParser.json());
+
+//* Might be useful if error blocked by cors
+// app.options('*', cors()); // include before other routes
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // or the specific domain you want to allow
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+
+// var permitCrossDomainRequests = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   // some browsers send a pre-flight OPTIONS request to check if CORS is enabled so you have to also respond to that
+//   if ('OPTIONS' === req.method) {
+//     res.send(200);
+//   }
+//   else {
+//     next();
+//   }
+// };
+
+// app.use(permitCrossDomainRequests);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
