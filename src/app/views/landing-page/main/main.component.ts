40dc685da3f47da0b8d4556454d2dd897c349e16
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-12-29 03:45:43 +0800

    update layout

--- src/app/views/landing-page/main/main.component.ts
+++ src/app/views/landing-page/main/main.component.ts
@@ -1,78 +1,3 @@
-// import { Component, OnInit, HostListener } from '@angular/core';;
-// import { Router } from '@angular/router';
-// import Swal from 'sweetalert2';
-// import { ProductService } from 'src/app/service/product-service'
-
-// @Component({
-//   selector: 'app-main',
-//   templateUrl: './main.component.html',
-//   styleUrls: ['./main.component.css']
-// })
-// export class MainComponent {
-//   currentProductIndex = 0;
-//   productList: any[] = [];
-//   scrollTimeout: any;
-//   buttonClicked = false;
-
-//   constructor(private router:Router, private productService: ProductService) {}
-
-//   ngOnInit(): void {
-//     this.productService.getAllProducts().subscribe(
-//       (products: any[]) => {
-//         this.productList = products;
-  
-//         // timeout 3s for scrolling if the user hasn't clicked on the button
-//         setTimeout(() => {
-//           if (!this.buttonClicked) {
-//             this.scrollToProducts();
-//           }
-//         }, 3000);
-//       },
-//       error => {
-//         console.error('Error fetching products:', error);
-//         // Handle the error, e.g., show an error message to the user
-//       }
-//     );
-//   }
-  
-//   scrollToProducts() {
-//     this.buttonClicked = true;
-//     const productSection = document.getElementById('productSection');
-
-//       if (productSection) {
-//         const offset = 0; //offset height of header
-//         const topPosition = productSection.getBoundingClientRect().top + window.scrollY;
-//           window.scrollTo({
-//             top: topPosition + offset,
-//             behavior: 'smooth',
-//           });
-//       }
-//   }
-
-//   cancelScrollTimeout() {
-//     clearTimeout(this.scrollTimeout);
-//   }
-
-//   scrollCarousel(direction: number) {
-//     const container = document.querySelector('.overflow-x-auto');
-//     if (container) {
-//       container.scrollBy({
-//         left: direction * 300, // Adjust the scroll distance as needed
-//         behavior: 'smooth',
-//       });
-//     }
-//   }
-
-//   goToProductDetail (productID: number) {
-//     this.router.navigate(['/product', productID]);
-//   }
-
-//   checkOutNow (productID: number) {
-//     this.router.navigate(['/customer/checkout', productID]);
-//   }
-
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'env/dev.environtment';
@@ -88,13 +13,17 @@ export class MainComponent implements OnInit {
   productList: any[] = [];
   scrollTimeout: any;
   buttonClicked = false;
+  isLoading = false;
 
   constructor(private router: Router, private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
+    //TODO: Get 5 products from the backend API
     this.productService.getAllProducts().subscribe(
       (products: any[]) => {
         this.productList = products;
+        this.isLoading = false;
 
         // Timeout 3s for scrolling if the user hasn't clicked on the button
         setTimeout(() => {
@@ -128,7 +57,7 @@ export class MainComponent implements OnInit {
 
   scrollToProducts() {
     this.buttonClicked = true;
-    const productSection = document.getElementById('productSection');
+    const productSection = document.getElementById('trending');
 
     if (productSection) {
       const offset = 0; // Offset height of the header
@@ -168,6 +97,10 @@ export class MainComponent implements OnInit {
         // Handle the error, e.g., show an error message to the user
       }
     );
+  }
+
+  goToAllProducts() {
+    this.router.navigate(['/all-products']);
   }
 
   checkOutNow(productID: number) {
