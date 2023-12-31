ee074d0e62760f39cac0cfef3ead6a1fecade561
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-12-31 22:11:58 +0800

    bugfix top 5 sold

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // Define routes for product-related operations
 router.post('/', productsController.createProduct);
 router.get('/', productsController.getProducts);
+router.get("/top-5-sold", productsController.getTop5Products);
 router.get('/:id', productsController.getProductById);
 router.patch('/:id', productsController.updateProduct);
 router.delete('/:id', productsController.deleteProduct);
@@ -13,7 +14,6 @@ router.get('/merchant/:merchantId', productsController.getProductsByMerchant);
 router.post('/:productId/reviews', productsController.addReview);
 router.put('/:productId/rating', productsController.updateRating);
 router.put('/:productId/soldQty', productsController.updateSoldQty);
-router.get("/top-5-sold", productsController.getTop5Products);
 router.get("/by-categories/:category", productsController.getProductsByCategories);
 
 export default router;
